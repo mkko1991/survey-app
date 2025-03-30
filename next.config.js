fd7d6ba0dab793aff26892a17b26b1e2265060ab
fito-minki <minki.ko@fitogether.com>
2025-03-30 15:40:54 +0900

    remove trash codes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,      // ✅ 각 페이지 경로에 `/` 붙게 함 (정적 라우팅에 좋음)
-    reactStrictMode: true,    // (선택) 개발 경고
-    swcMinify: true           // (선택) 빌드 속도 개선
+    trailingSlash: true,
 };
 
 module.exports = nextConfig;
